6866d9ce189f9e62a7432bbd82d1e9a6b3af584d
Serzh92 <55488341+Serzh92@users.noreply.github.com>
2019-09-30 21:32:55 +0300

    Add files via upload

---
+++ chast4/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$("#pokaz").click(function () {
+		$(".show").toggleClass("hide"); 
+	});
+});
+$(document).ready(function () {
+	$("#hidbut").click(function () {
+		$(".show").toggleClass("hide"); 
+	});
+});
